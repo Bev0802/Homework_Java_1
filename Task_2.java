8c07f3ddcdb0bcb2fc73681355de0e9b5b3a2023
Katy <bev08021982@gmail.com>
2023-02-13 02:49:19 +0400

    Решила задачу 3

--- Task_2.java
+++ Task_2.java
@@ -5,19 +5,18 @@ import java.util.List;
 public class Task_2 {
     public static void main(String[] args) {
         List<Integer> listPrimeNum = new ArrayList<Integer>();
-        int i= 2;
-        int count = 0;
-        for (int n = 2; n <= 50; n++) {
+        int i = 2, count = 0;
+        for (int n = 2; n <= 1000; n++) {
             count = 0;
-            while (i<n){
-                if (n%i==0){
-                    count++;
-                }
+            i = 2;
+            while (i < n) {
+                if (n % i == 0) count++;
+                i++;
             }
-            if (count==0){listPrimeNum.add(n);}
-            }
-    
+            if (count == 0) listPrimeNum.add(n);
+                    }
+// печть списка
         for (Integer j : listPrimeNum)
-            System.out.printf("%s ", j);
+            System.out.printf("%s ", j);        
     }
 }
--- Task_3.java
+++ Task_3.java
@@ -1,7 +1,53 @@
+
 //Реализовать простой калькулятор Введите число 2 Введите операцию 1 + 2 - 3 * 4 / 
 //Введите число 2 2 + 2 = 4
+import java.util.Scanner;
+
 public class Task_3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        // Получение данных от пользователя
+        System.out.printf("Введите первое число: ");
+        Scanner scx = new Scanner(System.in);
+        int x = scx.nextInt();
+
+        System.out.printf("Введите операцию +, -, *, /: ");
+        Scanner sc = new Scanner(System.in, "cp866");
+        String sign = sc.nextLine();
         
+        System.out.printf("Введите второе число: ");
+        Scanner scy = new Scanner(System.in);
+        int y = scx.nextInt();
+
+        scy.close();
+        scx.close();
+        sc.close();
+
+        int result = 0;
+        double res, xd, yd = 0.01;
+
+        if ((sign.equals("/"))) {
+            res = (double) result;
+            xd = (double) x;
+            yd = (double) y;
+            res = xd / yd;
+            System.out.printf("%s %s %s = %s", xd, sign, yd, res);
+        } 
+        else {
+            switch (sign) {
+                case "+":
+                result = x + y;
+                System.out.printf("%s %s %s = %s", x, sign, y, result);
+                    break;
+                case "-":
+                result = x - y;
+                System.out.printf("%s %s %s = %s", x, sign, y, result);
+                    break;
+                case "*":
+                result = x * y;
+                System.out.printf("%s %s %s = %s", x, sign, y, result);
+                    break;
+            }
+        }
     }
 }
