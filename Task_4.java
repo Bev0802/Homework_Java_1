0aa315de6e819cabeff1dcd75d08c0cca8357733
Katy <bev08021982@gmail.com>
2023-02-13 21:45:20 +0400

    Решила задачу 4

--- Task_4.java
+++ Task_4.java
@@ -1,7 +1,25 @@
+//import java.io.PrintStream;
+
+//import javax.naming.spi.DirStateFactory.Result;
+
 //*+Задано уравнение вида q + w = e, q, w, e >= 0. Некоторые цифры могут быть заменены знаком вопроса, например 2? + ?5 = 69. Требуется восстановить выражение до верного равенства. 
 //Предложить хотя бы одно решение или сообщить, что его нет.
 public class Task_4 {
- public static void main(String[] args) {
-    
- }   
+    // private static PrintStream printf;
+
+    public static void main(String[] args) {
+        int a = 20, b = 5, x = 0, y = 0, sum = 69;
+
+        for (int ax = 0; ax < 10; ax++) {
+            for (int ay = 0; ay <= 90; ay = ay + 10) {
+                if (((a + ax) + (ay + b)) == sum) {
+                    x = a + ax;
+                    y = ay + b;
+                    System.out.format("%d + %d = %d\n", x, y, sum);
+                }
+
+            }
+
+        }
+    }
 }
